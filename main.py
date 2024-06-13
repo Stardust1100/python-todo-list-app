b8a0126427ce1352fca1572c531504fcc6f26eae
Stardust1100 <158131496+Stardust1100@users.noreply.github.com>
2024-06-13 01:43:01 -0400

    Add files for Day1 todo app

---
+++ main.py
@@ -0,0 +1,11 @@
+
+prompt = "Enter a new todo item:"
+todo1 = input(prompt)
+todo2 = input(prompt)
+todo3 = input(prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+
+print(type(todo1))
+print(type(todos))
